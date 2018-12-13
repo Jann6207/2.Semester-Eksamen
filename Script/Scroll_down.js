7ce92a91bde2dc54440e461e91be8bdde3d1a787
jann6207 <jann6207@stud.kea.dk>
2018-12-13 04:45:57 +0100

    LandingPage = Modificeret

--- Script/Scroll_down.js
+++ Script/Scroll_down.js
@@ -1,13 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(function() {
-    $('.click-scroll').click (function() {
-      $('html, body').animate({scrollTop: $('#nyeste_vaerker_container').offset().top }, 'slow');
-      return false;
+    $(function () {
+        $('.click-scroll').click(function () {
+            $('html, body').animate({
+                scrollTop: $('#nyeste_vaerker_container').offset().top
+            }, 'slow');
+            return false;
+        });
     });
-  });
+
+
+    $(function () {
+        $('.arrow2').click(function () {
+            $('html, body').animate({
+                scrollTop: $('#bag_om_vaekert_container').offset().top
+            }, 'slow');
+            return false;
+        });
+    });
+
+
+
+    $(function () {
+        $('.arrow').click(function () {
+            $('html, body').animate({
+                scrollTop: $('#udstillinger').offset().top
+            }, 'slow');
+            return false;
+        });
+    });
+
+
 
 })
+
+
+
 
 
 
