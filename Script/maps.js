bb40e2e333c8e9bef9805d705801b1f6fcb75853
jann6207 <jann6207@stud.kea.dk>
2018-12-17 22:59:32 +0100

    Kontakt Side Done = Desktop _v4

---
+++ Script/maps.js
@@ -0,0 +1,30 @@
+
+
+    {
+        "mapouter": [
+            {
+                "hue": "#ff1a00"
+            },
+            {
+                "invert_lightness": true
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 33
+            },
+            {
+                "gamma": 0.5
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "mapouter": [
+            {
+                "color": "#2D333C"
+            }
+        ]
+    }
